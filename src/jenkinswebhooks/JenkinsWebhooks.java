bef5db28275c8d619709ab4e34d3935718ac9f05
Ana-ramahef <59668599+Ana-ramahef@users.noreply.github.com>
2021-09-29 17:45:14 -0400

    adding Au revoir

--- src/jenkinswebhooks/JenkinsWebhooks.java
+++ src/jenkinswebhooks/JenkinsWebhooks.java
@@ -16,6 +16,7 @@ public class JenkinsWebhooks {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[0]+", " +args[0]);
+        System.out.println("au revoir");
     }
     
 }
